b26adf8078b00499ae1be170c30ca0da7f8a7219
Maneesh63 <maneeshmaneesh391@gmail.com>
2024-05-24 08:16:51 +0530

    PF

---
+++ project/portfolio/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+
+def contact(request):
+    return render(request,'contact.html')
+
+
+def project(request):
+    return render(request,'project.html')
+
+
+
+def skills(request):
+    return render(request,'skills.html')
